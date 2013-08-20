649723eb65872ac631dfc3b38b66db49920724d8
stevepeak <steve@stevepeak.net>
2013-08-20 18:10:33 -0500

    staring to switch to BS3

--- bootstrap-panels.coffee
+++ bootstrap-panels.coffee
@@ -1,26 +1,26 @@
 $ ->
-  $.panels = (toggle) ->
+  $.windows = (toggle) ->
     if toggle
-      if $('.panel').hasClass toggle
-        $('.panel').removeClass "left right"
+      if $('.window').hasClass toggle
+        $('.window').removeClass "left right"
       else
-        $('.panel').addClass toggle
+        $('.window').addClass toggle
     else
       $('[data-toggle=slide]').click (event) ->
         event.preventDefault()
         target = $ $(@).attr 'data-target'
-        dir = if target.hasClass('panel-left') then 'right' else 'left'
+        dir = if target.hasClass('window-left') then 'right' else 'left'
         if target.hasClass(dir)
-          $('.panel.panel-center').removeClass 'left right'
+          $('.window.window-center').removeClass 'left right'
           target.removeClass dir
         else
-          $('.panel.panel-center').addClass dir
+          $('.window.window-center').addClass dir
           target.addClass dir
 
-      $('.panel:not(.panel-center) [data-toggle^="tab"]').click ->
+      $('.window:not(.window-center) [data-toggle^="tab"]').click ->
         # slide it back
-        $('.panel.in').removeClass 'in'
-        # force the panel to the top
-        $('.panel.panel-center').scrollTop 0
+        $('.window.in').removeClass 'in'
+        # force the window to the top
+        $('.window.window-center').scrollTop 0
 
-  $.panels()
+  $.windows()
--- bootstrap-panels.js
+++ bootstrap-panels.js
@@ -1,33 +1,33 @@
 (function() {
   $(function() {
-    $.panels = function(toggle) {
+    $.windows = function(toggle) {
       if (toggle) {
-        if ($('.panel').hasClass(toggle)) {
-          return $('.panel').removeClass("left right");
+        if ($('.window').hasClass(toggle)) {
+          return $('.window').removeClass("left right");
         } else {
-          return $('.panel').addClass(toggle);
+          return $('.window').addClass(toggle);
         }
       } else {
         $('[data-toggle=slide]').click(function(event) {
           var dir, target;
           event.preventDefault();
           target = $($(this).attr('data-target'));
-          dir = target.hasClass('panel-left') ? 'right' : 'left';
+          dir = target.hasClass('window-left') ? 'right' : 'left';
           if (target.hasClass(dir)) {
-            $('.panel.panel-center').removeClass('left right');
+            $('.window.window-center').removeClass('left right');
             return target.removeClass(dir);
           } else {
-            $('.panel.panel-center').addClass(dir);
+            $('.window.window-center').addClass(dir);
             return target.addClass(dir);
           }
         });
-        return $('.panel:not(.panel-center) [data-toggle^="tab"]').click(function() {
-          $('.panel.in').removeClass('in');
-          return $('.panel.panel-center').scrollTop(0);
+        return $('.window:not(.window-center) [data-toggle^="tab"]').click(function() {
+          $('.window.in').removeClass('in');
+          return $('.window.window-center').scrollTop(0);
         });
       }
     };
-    return $.panels();
+    return $.windows();
   });
 
 }).call(this);
